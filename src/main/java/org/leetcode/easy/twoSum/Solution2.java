6960e168d5dc7c113f0ebc73c3e2499762153c8f
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2026-02-24 12:47:51 -0600

    Add migration XML files and implement optimized twoSum solution with tests

--- src/main/java/org/leetcode/easy/twoSum/Solution2.java
+++ src/main/java/org/leetcode/easy/twoSum/Solution2.java
@@ -1,4 +1,23 @@
 package org.leetcode.easy.twoSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution2 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];                       // Número actual
+            int complement = target - num;           // Número que necesitamos
+
+            if (map.containsKey(complement)) {       // Si ya vimos el complemento
+                return new int[]{map.get(complement), i}; // Devolvemos los índices
+            }
+
+            map.put(num, i);                         // Guardamos el número actual
+        }
+
+        return new int[0]; // Nunca debería ocurrir por restricción
+    }
 }
--- src/test/java/org/leetcode/easy/twoSum/TwoSumTest.java
+++ src/test/java/org/leetcode/easy/twoSum/TwoSumTest.java
@@ -13,7 +13,7 @@ class TwoSumTest {
         assertArrayEquals(new int[]{0,1}, sol.twoSum(new int[]{3,3}, 6));
     }
 
-    /*@Test
+    @Test
     void testOptimized() {
         Solution2 sol = new Solution2();
         assertArrayEquals(new int[]{0,1}, sol.twoSum(new int[]{2,7,11,15}, 9));
@@ -25,6 +25,6 @@ class TwoSumTest {
     void testEdgeCases() {
         Solution2 sol = new Solution2();
         assertArrayEquals(new int[]{0,1}, sol.twoSum(new int[]{1,2}, 3));  // mínimo tamaño
-        assertArrayEquals(new int[]{0,1}, sol.twoSum(new int[]{-1,-2,-3,-4,-5}, -8));
-    }*/
+        assertArrayEquals(new int[]{2,4}, sol.twoSum(new int[]{-1,-2,-3,-4,-5}, -8));
+    }
 }
