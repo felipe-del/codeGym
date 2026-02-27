e83fc3d8fc5ea4f3d05304f72f2fc00f7c36a42c
Isaac Felipe Brenes Calderón <isaacfelibrenes1904@gmail.com>
2026-02-27 14:35:43 -0600

    Add StreamIntermediate class

---
+++ src/main/java/org/streams/exercises/StreamIntermediate.java
@@ -0,0 +1,9 @@
+package org.streams.exercises;
+
+public class StreamIntermediate {
+
+
+    public static void main(String[] args) {
+        System.out.println("Ahora le subimos el nivel a los streams :)");
+    }
+}
